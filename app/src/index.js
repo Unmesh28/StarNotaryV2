4a5dc7e5c2e720b46d6f88f2b17b1124ad1cb75d
Unmesh28 <unmesh.dabhade@gmail.com>
2022-06-26 10:34:10 +0530

    Updates

--- app/src/index.js
+++ app/src/index.js
@@ -54,5 +54,5 @@ window.addEventListener("load", async function() {
     App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
   }
 
-  //App.start();
+  App.start();
 });
